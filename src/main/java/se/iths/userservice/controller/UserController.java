790dd585d06a0a33ddd84abc4d2659927c95fa76
maxerkmar <max.erkmar@iths.se>
2024-05-08 14:21:17 +0200

    added user controller

--- src/main/java/se/iths/userservice/controller/UserController.java
+++ src/main/java/se/iths/userservice/controller/UserController.java
@@ -1,4 +1,40 @@
 package se.iths.userservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import se.iths.userservice.dto.UserDTO;
+import se.iths.userservice.service.UserService;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO getUserById(@PathVariable("id") Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/all")
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody UserDTO userDTO) {
+        userService.createUser(userDTO);
+    }
+
+    @PutMapping
+    public UserDTO updateUser(@RequestBody UserDTO userDTO) {
+        return userService.updateUser(userDTO);
+    }
+
 }
