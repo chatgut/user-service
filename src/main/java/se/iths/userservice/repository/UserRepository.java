24412b15bfea7dadbceb343a0a1f93c77857edce
maxerkmar <max.erkmar@iths.se>
2024-05-08 12:21:53 +0200

    add user repository

--- src/main/java/se/iths/userservice/repository/UserRepository.java
+++ src/main/java/se/iths/userservice/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package se.iths.userservice.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.ListCrudRepository;
+import se.iths.userservice.entities.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    @Query("SELECT u FROM UserEntity u WHERE u.username = :username")
+    UserEntity findByUsername(String username);
 }
