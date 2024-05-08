c0358a45e9194a940659752cb68a5d38aa4d7157
maxerkmar <max.erkmar@iths.se>
2024-05-08 12:52:05 +0200

    change in repository

--- src/main/java/se/iths/userservice/repository/UserRepository.java
+++ src/main/java/se/iths/userservice/repository/UserRepository.java
@@ -2,10 +2,10 @@ package se.iths.userservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.ListCrudRepository;
 import se.iths.userservice.entities.UserEntity;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    @Query("SELECT u FROM UserEntity u WHERE u.username = :username")
-    UserEntity findByUsername(String username);
+    Optional<UserEntity> findById(Long id);
 }
